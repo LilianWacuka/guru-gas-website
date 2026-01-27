0f0fc862418ff82a285696358ecac826b9250689
Kyalo <msbsam02@gmail.com>
2026-01-27 13:09:19 +0300

    Nimeunda database tables

--- backend/gurugasspoint/models.py
+++ backend/gurugasspoint/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name=models.CharField(max_length=100)
+    weight=models.IntegerField()
+    price=models.IntegerField()
+    units=models.IntegerField()
+    serial_number=models.IntegerField()
+    
+    
+class Order(models.Model):
+    Customer_name=models.CharField(max_length=100)
+    product_name=models.CharField(max_length=100)
+    weight=models.IntegerField()
+    quantity=models.IntegerField()
+    price=models.IntegerField()
+    
+class Customer(models.Model):
+    customer_name=models.CharField(max_length=100)
+    phone=models.IntegerField()
+    location=models.CharField(max_length=80)
+    
+class User(models.Model):
+    username=models.CharField(max_length=200)
+    password=models.CharField(max_length=100)
