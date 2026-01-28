ffcf669c55b35a81c9056249cb5aa770e033106e
Kyalo <msbsam02@gmail.com>
2026-01-28 23:47:07 +0300

    nimeeka zoote ziko poa bado user na customer update and delete

--- backend/gurugasspoint/models.py
+++ backend/gurugasspoint/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     price=models.IntegerField()
     units=models.IntegerField()
     serial_number=models.CharField(max_length=100)
+    product_image = models.ImageField(upload_to="static/images/",default='path/to/placeholder.jpg')
     def __str__(self):
         return self.product_name
     
@@ -17,6 +18,7 @@ class Order(models.Model):
     weight=models.IntegerField()
     quantity=models.IntegerField()
     price=models.IntegerField()
+    
     def __str__(self):
         return self.customer_name
     
@@ -24,11 +26,13 @@ class Customer(models.Model):
     customer_name=models.CharField(max_length=100)
     phone=models.IntegerField()
     location=models.CharField(max_length=80)
+    customer_image = models.ImageField(upload_to="static/images/",default='path/to/placeholder.jpg')
     def __str__(self):
         return self.customer_name
     
 class User(models.Model):
     username=models.CharField(max_length=200)
     password=models.CharField(max_length=100)
+    user_image = models.ImageField(upload_to="static/images/",default='path/to/placeholder.jpg')
     def __str__(self):
         return self.username
--- backend/gurugasspoint/views.py
+++ backend/gurugasspoint/views.py
@@ -1,12 +1,12 @@
 # myapp/views.py
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Product
-from .forms import ProductForm
+from .models import Product,Customer,User
+from .forms import ProductForm,CustomerForm,UserForm
 
 # READ: List all products
 def product_list(request):
     products = Product.objects.all()
-    return render(request, 'allproducts.html', {'products': products})
+    return render(request, 'products/allproducts.html', {'products': products})
 
 # CREATE: Add a new product
 def product_create(request):
@@ -14,7 +14,7 @@ def product_create(request):
     if form.is_valid():
         form.save()
         return redirect('product_list')
-    return render(request, 'addproducts.html', {'form': form})
+    return render(request, 'products/addproducts.html', {'form': form})
 
 # UPDATE: Edit an existing product
 def product_update(request, pk):
@@ -23,7 +23,7 @@ def product_update(request, pk):
     if form.is_valid():
         form.save()
         return redirect('product_list')
-    return render(request, 'editproducts.html', {'form': form})
+    return render(request, 'products/editproducts.html', {'form': form})
 
 # DELETE: Remove a product
 def product_delete(request, pk):
@@ -32,3 +32,67 @@ def product_delete(request, pk):
         product.delete()
         return redirect('product_list')
     return render(request, 'allproducts.html', {'product': product})
+
+#  MAMABO YA  CUSTOMER KWA DATABASE
+# READ: List all products
+def customer_list(request):
+    customers = Customer.objects.all()
+    return render(request, 'customers/allcustomers.html', {'customers': customers})
+
+# CREATE: Add a new customer
+def customer_create(request):
+    form = CustomerForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('customer_list')
+    return render(request, 'customers/addcustomer.html', {'form': form})
+
+# UPDATE: Edit an existing Customer
+def customer_update(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    form = CustomerForm(request.POST or None, instance=customer)
+    if form.is_valid():
+        form.save()
+        return redirect('customer_list')
+    return render(request, 'customers/editcustomer.html', {'form': form})
+
+# DELETE: Remove a Customer
+def customer_delete(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    if request.method == 'POST':
+        customer.delete()
+        return redirect('customers_list')
+    return render(request, 'customer/allcustomers.html', {'customer': customer})
+
+#  MAMABO YA  users KWA DATABASE
+# READ: List all USERS
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'users/allusers.html', {'users': users})
+
+# CREATE: Add a new user
+def user_create(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('user_list')
+    return render(request, 'users/adduser.html', {'form': form})
+
+# UPDATE: Edit an existing user
+def user_update(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    form = CustomerForm(request.POST or None, instance=user)
+    if form.is_valid():
+        form.save()
+        return redirect('user_list')
+    return render(request, 'users/edituser.html', {'form': form})
+
+# DELETE: Remove a user
+def user_delete(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == 'POST':
+        user.delete()
+        return redirect('users_list')
+    return render(request, 'users/allusers.html', {'user': user})
+
+
