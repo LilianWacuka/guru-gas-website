ed6cd357332c539d03a0fa8fad0f1572fd13273b
Lilian Wacuka <liliannguruw@gmail.com>
2026-01-29 15:10:45 +0300

    Delete backend/gurugasspoint directory
    Wrong merging so delete

--- backend/gurugasspoint/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from gurugasspoint .models import Product,Customer,User
-# Register your models here.
-admin.site.register (Product)
-admin.site.register (Customer)
-admin.site.register (User)
