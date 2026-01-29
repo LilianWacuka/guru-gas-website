8a826e9f3a45bc49c8c5bb9eca4b3185dcf61877
lilian <lilianguruw@gmail.com>
2026-01-29 10:07:42 +0300

    frontend changes

--- frontend/file.tsx
+++
@@ -1,3 +0,0 @@
-function userProfile() {
-    // generate your file here.
-}
---
+++ frontend/guru-gas-website/app/page.tsx
@@ -0,0 +1,9 @@
+import Product from './product/page';
+
+export default function HomePage() {
+  return (
+    <div> 
+      <Product />
+    </div>
+  )
+}
